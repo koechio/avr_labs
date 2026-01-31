d3baa72b4cbc16445bc59be9873e5afbb5678cea
Allan Koech <allan.koech959@gmail.com>
2026-01-31 16:23:42 -0600

    feat: add starter files and add a bunch of new functions for organ

---
+++ 02_serial_io/organ/organ.h
@@ -0,0 +1,2 @@
+void playNote(uint16_t period, uint16_t duration);
+void rest(uint16_t duration);
---
+++ 02_serial_io/organ/pinDefines.h
@@ -0,0 +1,4 @@
+#define SPEAKER                 PD6                            
+#define SPEAKER_PORT            PORTD
+#define SPEAKER_PIN             PIND
+#define SPEAKER_DDR             DDRD
